5f7b50609a0125c53e214061aa15ac98081a2209
haf-developer <kone@on.hidas.com>
2018-05-06 22:48:17 +0300

    Added files that were left out from last commit

---
+++ src/ghjavakoul/keksinimi.java
@@ -0,0 +1,18 @@
+package ghjavakoul;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class keksinimi extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+        
+        primaryStage.show();		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
