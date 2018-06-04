8793fd712af24975ada4686301485324d425fdbe
haf-developer <kone@on.hidas.com>
2018-06-04 22:52:05 +0300

    Node backend kopypaste

--- nodeapps/expressbe/myapp/app.js
+++ nodeapps/expressbe/myapp/app.js
@@ -1,30 +1,79 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require("express-session")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Ladataan Passport ja sen Local Strategia
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// Ladataan käyttäjien malli
+var User = require('./models/model.js').User;
+
+// Luodaan strategia joka hakee kannasta käyttäjän ja vertaa salasanaa,
+// jos salasana on sama, käyttäjä on autentikoitunut
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({username: username}).then(function (u) {
+      if (u && u.password === password) {
+        return done(null, u);
+      }
+      else {
+        return done(null, false);
+      }
+    });
+  }));
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
+
+// Sarjallistetaan käyttäjän tietokanta id sessioon
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+// muunnetaan sessiossa oleva tietokanta id käyttäjäksi
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Käytetään sessioita sovelluksessa
+app.use(session({ secret: "cats" , resave: false,
+  saveUninitialized: true}));
+
+// alusteteaan passport käyttämään sovelluksen sessioita
+// sessiot pitää olla käytössä ennen tätä
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- nodeapps/expressbe/myapp/routes/index.js
+++ nodeapps/expressbe/myapp/routes/index.js
@@ -1,9 +1,81 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// ladataan passport jotta sitä voidaan käyttää tässä tiedostossa
+var passport = require('passport')
+
+// ladataan tietokanta mallit
+var Vastaus = require('../models/model.js').Vastaus;
+var User = require('../models/model.js').User;
+
+// ladataan uudelleen ohjauksiin kirjasto
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+
+
+// kaikissa poluissa paitsi loginissa ja registerissä käytetään ensureLoggedIn middlewarea
+// joka pakottaa sisäänkirjautumaan
+router.get('/', ensureLoggedIn('/login'), function(req, res, next) {
+  res.render('index');
 });
+
+router.post('/supersankari', ensureLoggedIn('/login'), function(req, res){
+
+  var nimi = req.body.nimi;
+  var supersankari = req.body.supersankari;
+
+  Vastaus.create({
+      nimi: nimi,
+      supersankari: supersankari
+    }).then(function() {
+      res.render('index', {nimi: req.body.nimi, supersankari: req.body.supersankari})
+    },
+    function() {
+      res.status(500).send("Tietokantavirhe");
+    });
+
+
+})
+
+router.get("/vastaukset", ensureLoggedIn('/login'), function(req, res) {
+    Vastaus.find(function (err, vastaukset) {
+    if (err) return console.error(err);
+    console.log(vastaukset);
+    res.render('vastaukset', {vastaukset: vastaukset});
+  });
+});
+
+
+// renderöidään register.ejs
+router.get('/register', function(req, res){
+  res.render('register');
+})
+
+router.post('/register', function(req, res){
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.create({
+      username: username,
+      password: password
+    }).then(function() {
+      res.render('login')
+    },
+    function() {
+      res.status(500).send("Tietokantavirhe");
+    });
+})
+
+// renderöidään login.ejs
+router.get('/login', function(req, res){
+  res.render('login');
+})
+
+// Autentikoidutaan passportin avulla jonka konfiguraatio on määritetty app.js:ssä
+router.post('/login', passport.authenticate('local',
+         {successReturnToOrRedirect: '/',
+          failureRedirect: '/login'}))
+
 
 module.exports = router;
